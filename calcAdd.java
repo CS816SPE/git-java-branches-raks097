78d87a15d39b472f9c62b0f72999feb02a2652e0
Rakshith <Sai.Rakshith@iiitb.org>
2019-01-17 10:12:10 +0530

    Source Code

---
+++ calcAdd.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class calcAdd
+{
+	public static void main (String args[])
+	{
+		float a,b,res;
+		char choice , ch;
+		Scanner scan = new Scanner(System.in);
+
+		do
+		{
+			System.out.println("1. ADD");
+			System.out.println("2. EXIT\n");
+			System.out.print("Enter Your Choice: " );
+			choice = scan.next().charAt(0);
+
+			switch(choice)
+			{
+				case '1' :
+					System.out.print("Enter Two Number : ");
+					a = scan.nextFloat();
+					b = scan.nextFloat();
+					res = a + b;
+					System.out.println("Result = "+res);
+					break;
+
+				case '2' :
+					System.exit(0);
+					break;
+
+				default : 
+					System.out.print("INVALID");
+					break;
+			}
+
+			System.out.println("----------------------------------------");
+		}
+
+		while(choice != '2');
+	}
+}
